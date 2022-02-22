ec1dc8ffcaa458a05f240c1dae0308b1bd40e910
ispoon21 <isaiahweathersp@gmail.com>
2022-02-22 11:30:37 -0700

    slight revision to frontend

--- src/components/menu/Itemcard.js
+++ src/components/menu/Itemcard.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Itemcard = (props) => {
 //   const { addItem } = useCart();
-
     
     return (
             <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
@@ -10,7 +9,7 @@ const Itemcard = (props) => {
                   <img src={props.image} className="card-img-top img-fluid" alt="food img" />
                      <div className="card-body text-center">
                         <h5 className="card-title">{props.name}</h5>
-                        <h5 className="card-text">{props.price}</h5>
+                        <h5 className="card-text">${props.price}</h5>
                         <p className="card-text">{props.description}</p>
                         <button className="btn3  size1 txt11 trans-0-4" onClick={()=> props.addItem(props.item)}>Add to Cart</button>
                     </div>
